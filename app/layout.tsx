02b50fb6a5196a9155e46178c9032a24944e543f
nobuhiro0202 <Nobu.s1322@gmail.com>
2024-02-19 23:47:09 +0900

    aergae

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
-import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Open_Sans } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs'
-import "./globals.css";
-import { ThemeProvider } from "@/components/providers/theme-provider"
-import { cn } from "@/lib/utils";
+
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { ModalProvider } from '@/components/providers/modal-provider';
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -30,6 +32,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
